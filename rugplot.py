42715102a687e034b51d7733944ca1755f8a12bc
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-07 20:19:20 +0530

    added rugplots

---
+++ rugplot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+flights_df=sns.load_dataset('flights')
+print(flights_df.head())
+
+passengers=flights_df['passengers']
+
+sns.rugplot(flights_df['passengers'])
+plt.show()
