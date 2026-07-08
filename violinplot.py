c1c130ddab8969b493fcd0418cc2dd2776104f62
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-08 20:17:59 +0530

    Added Violin plots

---
+++ violinplot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+flights_df=sns.load_dataset('tips')
+
+
+passengers=flights_df['day']
+year=flights_df['total_bill']
+sex=flights_df['sex']
+sns.violinplot(x=passengers,y=year,data=flights_df,hue=sex,split=True)
+plt.show()
